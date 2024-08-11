9e9c5ea190e5a6adf7d0b14142071584ebc518b6
priyeshkadbe <priyeshkadbe49@gmail.com>
2024-08-12 01:19:49 +0530

    update:title

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,5 @@
 export const TITLE =
-  "Fetch Balances for Bitcoin , Ethereum, Polygon and Binance Smart Chain";
+  "Fetch Balances and NFTs for Bitcoin , Ethereum, Polygon and Binance Smart Chain";
 
 
 export const API_URL = "https://tantum-backend-api.onrender.com/api/v1/balances";
